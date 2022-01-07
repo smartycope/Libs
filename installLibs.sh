12b7acfd9c70144a2bacceabd98e7a3f1074b769
smartycope <smartycope@gmail.com>
2022-01-06 19:30:26 -0700

    ommit

--- installLibs.sh
+++ installLibs.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 echo Installing Libs...
-export PYTHONPATH=${PYTHONPATH}:${HOME}/libs/Python
-export C_INCLUDE_PATH=${C_INCLUDE_PATH}:${HOME}/libs/C
-export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:${HOME}/libs/C++
+libsPath=${HOME}/hello/Libs
+export PYTHONPATH=${PYTHONPATH}:$libsPath/Python
+export C_INCLUDE_PATH=${C_INCLUDE_PATH}:$libsPath/C
+export CPLUS_INCLUDE_PATH=${CPLUS_INCLUDE_PATH}:$libsPath/C++
+echo Finished Installing Libs
+# export PYTHONPATH="$PYTHONPATH:$HOME/Libs/Python"
 
 # Q is the charge?
 # 1 coulomb/second = 1 Amp
